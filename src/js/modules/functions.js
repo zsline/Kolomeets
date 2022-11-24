e27885dcda4231cb5f7dc460ae350e1fbece92e6
zsline <sergei.dereka@gmail.com>
2022-11-24 10:05:23 +0200

    sidebar-mobil

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -20,12 +20,43 @@ document.querySelector('.header__nav').addEventListener('click', () => {
     const menuBlock = document.querySelector('.wrapper-nav')
     const menuIcon = document.querySelector('.header__nav')
     menuBlock.classList.toggle('nav-active');
-    if(menuBlock.classList.contains('nav-active')){
+    if (menuBlock.classList.contains('nav-active')) {
         menuIcon.classList.remove('icon-app-menu');
         menuIcon.classList.add('icon-close');
     } else {
         menuIcon.classList.remove('icon-close');
         menuIcon.classList.add('icon-app-menu');
     }
-    
+
 });
+
+
+// sidebar
+
+const titleSidebar = document.querySelectorAll('.sidebar__title');
+const sidebarTitle = document.querySelector('.sidebar__catalog-title');
+const filterTitle = document.querySelector('.sidebar__catalog-filter');
+const sidebarMenu = document.querySelector('.sidebar__catalog-nav');
+const sidebaFilter = document.querySelector('.sidebar__filter-wrapper');
+function sidebarActived(title, box) {
+    if (title || box) {
+        title.addEventListener('click', () => {
+            box.classList.toggle('active');
+            title.children[1].classList.toggle('rotate-active')
+            console.dir(title);
+        });
+    } else {
+        return
+    }
+}
+    titleSidebar.forEach((el) => {
+        el.firstElementChild.addEventListener('click', () => {
+            console.dir(el.firstElementChild.children[0]);
+            el.lastElementChild.classList.toggle('active')
+            el.firstElementChild.children[0].classList.toggle('rotate-active')
+        })
+    });
+
+
+sidebarActived(sidebarTitle, sidebarMenu)
+sidebarActived(filterTitle, sidebaFilter)
