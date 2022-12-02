99b38d57e1762e6f1c8c00d1a2d6af9b7dd55c53
zsline <sergei.dereka@gmail.com>
2022-12-02 14:21:38 +0200

    modal

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 import * as flsFunctions from "./modules/functions.js";
 import Swiper, { Navigation, Pagination } from 'swiper';
 import DerTabs from "./modules/DerTabs.js";
+import Modal from "./modules/Modal.js";
 
 flsFunctions.isWebp();
 
@@ -10,5 +11,15 @@ new DerTabs('tab', {
     isChanget: (tabs) => {
         // console.log(tabs);
     }
+});
+
+new Modal({
+    isOpen: (modal) => {
+        // console.log(modal);
+        // console.log('opened');
+    },
+    isClose: (modal) => {
+        // console.log('closed');
+    },
 });
 
