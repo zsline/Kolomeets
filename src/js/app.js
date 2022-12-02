d51a60d7fd63aa80a98d06bdf57e972d347546ee
zsline <sergei.dereka@gmail.com>
2022-12-02 12:52:12 +0200

    DerTabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,14 @@
 
 import * as flsFunctions from "./modules/functions.js";
 import Swiper, { Navigation, Pagination } from 'swiper';
+import DerTabs from "./modules/DerTabs.js";
 
 flsFunctions.isWebp();
 
 const swiper = new Swiper();
+new DerTabs('tab', {
+    isChanget: (tabs) => {
+        // console.log(tabs);
+    }
+});
 
